7d395ff7c6a1698154b54096047f28e08b536b08
Joe Fioti <jafioti@gmail.com>
2024-03-21 00:30:01 -0400

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,93 @@
-use dfdx::{
-    losses::mse_loss,
-    nn::{Linear, ModuleBuilder, ModuleMut, ReLU, Tanh},
-    optim::{Momentum, Optimizer, Sgd, SgdConfig},
-    shapes::{Rank2, Rank1},
-    tensor::{AsArray, Cpu, SampleTensor, Tensor1D, TensorFromArray},
-    tensor_ops::Backward,
-};
-use num::{Float, Zero};
+use dfdx::prelude::*;
+use num::{Float, One, Zero};
 use rand::Rng;
 
-// first let's declare our neural network to optimze
-type Mlp = (
-    (Linear<18, 100>, ReLU),
-    (Linear<100, 256>, ReLU),
-    (Linear<256, 100>, ReLU),
-    (Linear<100, 8>, Tanh),
+type Model = (
+    (LinearConstConfig<17, 50>, Tanh), // 17 = 8 bit input 1 + 8 bit input 2 + 1 bit opcode (add or subtract)
+    (LinearConstConfig<50, 50>, Tanh),
+    (LinearConstConfig<50, 8>, Tanh), // 8 = 8 bit wrapped output
 );
+
+const BATCH_SIZE: usize = 10;
 
 fn main() {
     let dev: Cpu = Default::default();
-
-    let mut sgd: Sgd<Mlp> = Sgd::new(SgdConfig {
-        lr: 1e-3,
-        momentum: Some(Momentum::Nesterov(0.9)),
-        weight_decay: None,
-    });
-
-    // let's initialize our model and some dummy data
-    let mut mlp: Mlp = dev.build_module();
+    let mut model = dev.build_module(Model::default());
+    let mut opt = Adam::new(
+        &model,
+        AdamConfig {
+            lr: 1e-3,
+            ..Default::default()
+        },
+    );
+    let mut grads = model.alloc_grads();
     let mut rng = rand::thread_rng();
-    let mut loss_avg = ExponentialAverage::new();
+    let mut loss_avg = ExponentialAverage::default();
 
-    for i in 0..1_000_000 {
-        let n1 = rng.gen::<u8>();
-        let n2 = rng.gen::<u8>();
-        let mut op = rng.gen_range(0..1);
-        if n2 == 0 && op == 3 {
-            op = rng.gen_range(0..1); // Ensure no divide by 0
-        }
-        let (input, target) = get_data(&dev, n1, n2, op);
-        let prediction = mlp.forward_mut(input.trace());
-        
+    let mut iter = 0;
+    // training loop
+    while loss_avg.value > 1e-3 {
+        iter += 1;
+        // generate problem
+        let (input, target) = get_data(&dev, rng.gen(), rng.gen(), rng.gen_range(0..2));
+        let prediction = model.forward_mut(input.trace(grads));
+
         let loss = mse_loss(prediction, target.clone());
         loss_avg.update(loss.array());
-        println!("Loss after update {i}: {:?}", loss_avg.value);
-        let gradients = loss.backward();
-        sgd.update(&mut mlp, gradients).unwrap();
+        grads = loss.backward();
+        if iter % BATCH_SIZE == 0 {
+            println!("Loss after update {iter}: {:?}", loss_avg.value);
+            opt.update(&mut model, &grads).unwrap();
+            model.zero_grads(&mut grads);
+        }
     }
+    println!("Learned after {iter} examples",)
 }
 
-fn get_data(dev: &Cpu, n1: u8, n2: u8, op: u8) -> (Tensor1D<18>, Tensor1D<8>) {
-    let (opcode1, opcode2, ans) = match op {
-        0 => (false, false, n1.wrapping_add(n2)),
-        1 => (false, true, n1.wrapping_sub(n2)),
-        2 => (true, false, n1.wrapping_mul(n2)),
-        _ => (true, true, n1.wrapping_div(n2)),
+fn get_data(dev: &Cpu, n1: u8, n2: u8, op: u8) -> (Tensor1D<17>, Tensor1D<8>) {
+    let (opcode, ans) = match op {
+        // Addition
+        0 => (1.0, n1.wrapping_add(n2)),
+        // Subtraction
+        1 => (-1.0, n1.wrapping_sub(n2)),
+        _ => unreachable!(),
     };
-    let mut v = get_bits(n1).to_vec();
-    v.append(&mut get_bits(n2).to_vec());
-    v.insert(0, opcode2);
-    v.insert(0, opcode1);
-    let v: Vec<_> = v.into_iter().map(|v| if v {-1.0} else {1.0}).collect();
+    let mut p = [0.; 17];
+    get_bits(n1, &mut p);
+    get_bits(n2, &mut p[8..]);
+    p[16] = opcode;
 
-    let a: Vec<_> = get_bits(ans).iter().copied().map(|v| if v {-1.0} else {1.0}).collect();
-
-    (
-        dev.tensor(TryInto::<[f32; 18]>::try_into(v).unwrap()),
-        dev.tensor(TryInto::<[f32; 8]>::try_into(a).unwrap()),
-    )
+    let mut a = [0.; 8];
+    get_bits(ans, &mut a);
+    (dev.tensor(p), dev.tensor(a))
 }
 
-fn get_bits(byte: u8) -> [bool; 8] {
-    let mut arr = [false; 8];
+fn get_bits(byte: u8, slice: &mut [f32]) {
+    #[allow(clippy::needless_range_loop)]
     for i in 0..8 {
-        arr[i] = byte >> i & 1 == 1;
+        slice[i] = if byte >> i & 1 == 1 { 1.0 } else { -1.0 };
     }
-
-    arr
 }
 
 pub struct ExponentialAverage<T: Float> {
     beta: f64,
     moment: f64,
     pub value: T,
-    t: i32
+    t: i32,
 }
 
-impl <T: Float> Default for ExponentialAverage<T> {
+impl<T: Float> Default for ExponentialAverage<T> {
     fn default() -> Self {
         ExponentialAverage {
-            beta: 0.99,
+            beta: 0.999,
             moment: 0.,
-            value: Zero::zero(),
-            t: 0
+            value: One::one(),
+            t: 0,
         }
     }
 }
 
-impl <T: Float> ExponentialAverage<T> {
-    pub fn new() -> Self {
-        ExponentialAverage {
-            beta: 0.99,
-            moment: 0.,
-            value: Zero::zero(),
-            t: 0
-        }
-    }
-
-    pub fn with_beta(beta: f64) -> Self {
-        assert!((0. ..=1.).contains(&beta));
-        ExponentialAverage {
-            beta,
-            moment: 0.,
-            value: Zero::zero(),
-            t: 0
-        }
-    }
-
+impl<T: Float> ExponentialAverage<T> {
     pub fn update(&mut self, value: T) {
         self.t += 1;
         self.moment = self.beta * self.moment + (1. - self.beta) * value.to_f64().unwrap();
